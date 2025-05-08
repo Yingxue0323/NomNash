1ffaf51ee09b75a1687af6e239bb68394d49ef1b
Alex <atra0068@student.monash.edu>
2025-05-08 18:36:53 +1000

    Business Profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { StarRatingComponent } from "./components/star-rating/star-rating.compon
 import { LaunchPageComponent } from "./pages/launch-page/launch-page.component"
 import { SignUpPageComponent } from "./pages/sign-up-page/sign-up-page.component"
 import { StudentProfileComponent } from "./pages/student-profile/student-profile.component"
+import { BusinessProfileComponent } from "./pages/business-profile/business-profile.component"
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { StudentProfileComponent } from "./pages/student-profile/student-profile
       { path: "", component: LaunchPageComponent },
       { path: "signup", component: SignUpPageComponent },
       { path: "student", component: StudentProfileComponent},
+      { path: "business", component: BusinessProfileComponent},
       { path: "**", redirectTo: "" },
     ]),
   ],
--- src/app/pages/business-profile/business-profile.component.ts
+++ src/app/pages/business-profile/business-profile.component.ts
@@ -1,12 +1,97 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { StarRatingComponent } from 'src/app/components/star-rating/star-rating.component';
+
+interface BusinessProfile { // Business Profile object containing all relevant variables 
+  name: string;
+  email: string;
+  role: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-business-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, StarRatingComponent],
   templateUrl: './business-profile.component.html',
-  styleUrl: './business-profile.component.scss'
+  styleUrls: ['./business-profile.component.scss']
 })
-export class BusinessProfileComponent {
 
+export class BusinessProfileComponent {
+  isEditing = false; // Track when profile is in edit mode 
+  businessProfile: BusinessProfile = { // Object holding business profile details, all initialised with default values
+    name: 'John Doe',
+    email: 'johndoe@business.monash.edu',
+    role: 'Business',
+    password: '********'
+  };
+
+  favouriteRestaurants: string[] = [
+    "Restaurant 1",
+    "Restaurant 2",
+    "Restaurant 3",
+    "Restaurant 4"
+  ];
+
+  pastReviews = [
+    {
+      restaurantName: "Peri Peri Chicken",
+      rating: 4,
+      text: "Chicken!",
+    },
+
+    {
+      restaurantName: "Guzman Y Gomez",
+      rating: 5,
+      text: "Mexican Dishes!",
+    },
+
+    {
+      restaurantName: "Peri Peri Chicken",
+      rating: 4,
+      text: "Chicken!",
+    },
+
+    {
+      restaurantName: "Guzman Y Gomez",
+      rating: 5,
+      text: "Mexican Dishes!",
+    },
+  ];
+
+  businessList = [
+    {
+      restaurantName: "Peri Peri Chicken",
+      rating: 4,
+      text: "(Restaurant Info Here)",
+    },
+
+    {
+      restaurantName: "Peri Peri Chicken",
+      rating: 4,
+      text: "(Restaurant Info Here)",
+    },
+
+    {
+      restaurantName: "Peri Peri Chicken",
+      rating: 4,
+      text: "(Restaurant Info Here)",
+    },
+    
+    {
+      restaurantName: "Peri Peri Chicken",
+      rating: 4,
+      text: "(Restaurant Info Here)",
+    },
+  ]
+
+  toggleEdit(): void { // Toggles editing depending on boolean value 
+    this.isEditing = !this.isEditing;
+  }
+
+  saveChanges(): void { // Save changes function
+    this.isEditing = false;
+    console.log('Profile updated:', this.businessProfile);
+  }
 }
